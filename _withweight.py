1acd35d776e9328782b6cfbec1bf75b7cf214267
Alan Liu <yliu908@ucr.edu>
2024-03-22 10:24:29 -0700

    wrapup

---
+++ _withweight.py
@@ -0,0 +1,48 @@
+import pandas as pd
+import networkx as nx
+from pyvis.network import Network
+import random
+
+# Load  data
+df = pd.read_csv('grouped_updated_combined_data.csv', low_memory=False)
+
+
+df = df.dropna(subset=['author', 'parent_post_author'])
+df = df[df['author'].apply(lambda x: isinstance(x, str))]
+df = df[df['parent_post_author'].apply(lambda x: isinstance(x, str))]
+df = df[(df['author'] != '-') & (df['author'] != "[deleted]")]
+df = df[(df['parent_post_author'] != '-') & (df['parent_post_author'] != "[deleted]")]
+G = nx.DiGraph()
+
+for _, row in df.iterrows():
+    source = row['parent_post_author']
+    target = row['author']
+    if G.has_edge(source, target):
+        G[source][target]['weight'] += 1
+    else:
+        G.add_edge(source, target, weight=1)
+
+
+sample_size = 500  #  sample size
+ 
+if len(G.nodes()) > sample_size:
+    sampled_nodes = random.sample(list(G.nodes()), sample_size)
+else:
+    sampled_nodes = G.nodes()
+    
+
+H = G.subgraph(sampled_nodes)
+
+nt = Network(notebook=False, height="750px", width="100%", bgcolor="#222222", font_color="white")
+for node in H.nodes:
+    nt.add_node(node)
+for edge in H.edges(data=True):
+    weight = edge[2]['weight']
+    nt.add_edge(edge[0], edge[1], width=weight)
+
+nt.from_nx(H)
+
+html_out = nt.generate_html()
+
+with open('withweight.html', 'w') as html_file:
+    html_file.write(html_out)
--- mixed.py
+++ mixed.py
@@ -12,7 +12,7 @@ df = df[df['parent_post_author'].apply(lambda x: isinstance(x, str))]
 df = df[df['group_per_month'].between(13, 36)]
 
 # Sample or limit to first 10000 rows if needed (remove or comment out if not desired)
-# df = df.sample(n=1000)
+df = df.sample(n=4000)
 # df = df.head(10000)
 
 G = nx.DiGraph()
@@ -28,4 +28,4 @@ for _, row in df.iterrows():
 
 fig = gv.d3(G, use_node_size_normalization=False, edge_label_data_source='weight', show_node_label=False, show_edge_label=True, edge_curvature=0.0,
       use_edge_size_normalization=True, zoom_factor=0.5)
-fig.export_html('final.html')
+fig.export_html('__mixed.html')
